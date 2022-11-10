1dda0d3be3af736f8e6dc9458b4cf117ea7bcd12
kishiym <kishiym@g.neec.ac.jp>
2022-11-10 11:32:50 +0900

    プロジェクト ファイルを追加します。

---
+++ Project6/ソース.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//[手順1] 単方向リストの構造体の定義
+typedef struct cell
+{
+    int val;
+    cell* next;
+
+} CELL;
+
+
+void create(CELL *p,int val); //プロトタイプ宣言 自分で作ってね
+void index();  //プロトタイプ宣言 自分で作ってね
+
+int main() {
+    int val;
+    //[手順2] 先頭のセルの宣言
+    CELL head;
+    head.next = nullptr; //Visual Studioではnextに何らかの値が入った状態で初期化されるので、nullptrを代入する
+
+    while (1) {
+        scanf_s("%d", &val);
+
+        //[手順3] 最後尾にセルを追加
+        create(&head, val);
+
+        //[手順4] リスト一覧の表示
+        index(&head);
+
+    }
+    return 0;
+}
+
+void create(CELL* endCell, int val)
+{
+    CELL* newCell;
+    newCell = (CELL*)malloc(sizeof(CELL));
+
+    newCell->val = val;
+    newCell->next = NULL;
+
+    while (endCell->next != NULL)
+    {
+        endCell = endCell->next;
+    }
+    endCell->next = newCell;
+}
+
+void index(CELL* endCell)
+{
+    while (endCell->next != NULL) 
+    {
+        endCell = endCell->next;
+        printf("%d\n", endCell->val);
+    }
+}
